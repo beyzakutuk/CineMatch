e46c5dcf3f7d584c71d7813c9d3d0f720fb87db0
Beyza Kütük <beyzakutuk55@gmail.com>
2025-07-27 01:44:17 +0300

    feat: added email and password validation rules

--- api/auth.py
+++ api/auth.py
@@ -2,17 +2,32 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, validator
 from auth.utils import hash_password, create_access_token
 from db.model import User
 from db.database import get_db
+import re
 
 router = APIRouter()
 
 class RegisterRequest(BaseModel):
-    email: str
+    email: EmailStr
     username: str
     password: str
+    
+    @validator("password")
+    def validate_password(cls, value):
+        if len(value) < 8:
+            raise ValueError("Şifre en az 8 karakter olmalıdır.")
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("Şifre en az bir büyük harf içermelidir.")
+        if not re.search(r"[a-z]", value):
+            raise ValueError("Şifre en az bir küçük harf içermelidir.")
+        if not re.search(r"\d", value):
+            raise ValueError("Şifre en az bir rakam içermelidir.")
+        if not re.search(r"[!@#$%^&*()_\+\-=\[\]{}|;:'\",.<>?/]", value):
+            raise ValueError("Şifre en az bir özel karakter içermelidir.")
+        return value
     
 @router.post("/register")
 async def register_user(user_data: RegisterRequest, db: AsyncSession = Depends(get_db)):
