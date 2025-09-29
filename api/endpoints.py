36ce879d948678cd780e4f18be143abf2d57170e
Beyza Kütük <beyzakutuk55@gmail.com>
2025-09-29 22:31:22 +0300

    feat: added recommendation endpoint based on user favorites

--- api/endpoints.py
+++ api/endpoints.py
@@ -11,6 +11,8 @@ from db.database import get_db
 from auth.dependencies import get_current_user
 from schemas.title_schema import TitlesResponse
 
+from recommender import recommender
+
 router = APIRouter()
 
 @router.get("/titles/", response_model=TitlesResponse)
@@ -39,6 +41,22 @@ async def search_titles(
     total = len(titles)
     return {"total": total, "titles": titles}
 
+@router.get("/recommendations/favorites/")
+async def recommend_by_favorites( n: int = Query(5, description="gösterilecek içerik sayısı"), db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+    result = await db.execute(
+        select(Title.name).join(Favorite, Favorite.title_id == Title.id).where(Favorite.user_id == current_user.id)
+    )
+    
+    favorite_titles = [row[0] for row in result.all()]
+    if not favorite_titles:
+        return {"message": "favorilenen içerik bulunamadı"}
+    
+    recommendations = recommender.recommend_by_user_favorites(favorite_titles, n)
+    
+    return{
+        "based_on_favorites": favorite_titles,
+        "recommendations": recommendations
+    }
 
 @router.post("/favorites/")
 async def add_favorite(title_id: int, db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
@@ -75,3 +93,4 @@ async def remove_favorite(title_id: int, db: AsyncSession=Depends(get_db), curre
     await db.delete(favorite)
     await db.commit()
     return {"message": "Favori Silindi."}
+
